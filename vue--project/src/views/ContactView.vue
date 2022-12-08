6488e79a1cd6a6acb32092aa95fc7687c0c6755a
Blessy Kuttappan <blessy.kuttappan@focaloid.com>
2022-12-08 13:49:26 +0530

    final commit

--- vue--project/src/views/ContactView.vue
+++ vue--project/src/views/ContactView.vue
@@ -4,18 +4,18 @@
     <div class="container">
       <div class="footer_main">
         <div class="footer_left">
-          <h1 class="contact_taital"><span>Contact </span> <img src="src/assets/images/icon-2.png"> <span>Us</span></h1>
+          <h1 class="contact_taital"><span>Contact </span> <img src="../assets/images/icon-2.png"> <span>Us</span></h1>
         </div>
         <div class="footer_left">
-          <div class="location_text"><a href="#"><img src="src/assets/images/map-icon.png"><span
+          <div class="location_text"><a href="#"><img src="../assets/images/map-icon.png"><span
                 class="padding_left_15">Locations</span></a></div>
         </div>
         <div class="footer_left">
-          <div class="location_text"><a href="#"><img src="src/assets/images/call-icon.png"><span
+          <div class="location_text"><a href="#"><img src="../assets/images/call-icon.png"><span
                 class="padding_left_15">+71 9876543210</span></a></div>
         </div>
         <div class="footer_left">
-          <div class="location_text"><a href="#"><img src="src/assets/images/map-icon.png"><span
+          <div class="location_text"><a href="#"><img src="../assets/images/map-icon.png"><span
                 class="padding_left_15">demo@gmail.com</span></a></div>
         </div>
       </div>
@@ -41,10 +41,10 @@
             </div>
             <div class="social_icon">
               <ul>
-                <li><a href="#"><img src="src/assets/images/fb-icon1.png"></a></li>
-                <li><a href="#"><img src="src/assets/images/twitter-icon1.png"></a></li>
-                <li><a href="#"><img src="src/assets/images/linkden-icon1.png"></a></li>
-                <li><a href="#"><img src="src/assets/images/instagram-icon1.png"></a></li>
+                <li><a href="#"><img src="../assets/images/fb-icon1.png"></a></li>
+                <li><a href="#"><img src="../assets/images/twitter-icon1.png"></a></li>
+                <li><a href="#"><img src="../assets/images/linkden-icon1.png"></a></li>
+                <li><a href="#"><img src="../assets/images/instagram-icon1.png"></a></li>
               </ul>
             </div>
           </div>
